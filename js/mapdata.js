a45bcc6277be39d14c56dcb6b47778f02560bc89
Nihal Awasthi <137594290+nihalawasthi@users.noreply.github.com>
2024-04-09 20:27:27 +0530

    Add files via upload

---
+++ js/mapdata.js
@@ -0,0 +1,193 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+    width: "responsive",
+    background_color: "#252525",
+    border_color: "#8e8e8e",
+    
+    state_description: "State description",
+    state_color: "#FFFFFF",
+    state_hover_color: "#8e8e8e",
+    state_url: "",
+    border_size: "1",
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    location_description: "Location description",
+    location_color: "#000000",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 15,
+    location_type: "circle",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 12,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    border_hover_color: "#000000",
+    border_hover_size: "2"
+  },
+  state_specific: {
+    "1": {
+      name: "Andaman And Nicobar"
+    },
+    "2": {
+      name: "Andhra Pradesh"
+    },
+    "3": {
+      name: "Arunachal Pradesh"
+    },
+    "4": {
+      name: "Assam"
+    },
+    "5": {
+      name: "Bihar"
+    },
+    "6": {
+      name: "Chandigarh"
+    },
+    "7": {
+      name: "Chhattisgarh"
+    },
+    "8": {
+      name: "Dadra And Nagar Haveli"
+    },
+    "9": {
+      name: "Daman And Diu"
+    },
+    "10": {
+      name: "Delhi"
+    },
+    "11": {
+      name: "Goa"
+    },
+    "12": {
+      name: "Gujarat"
+    },
+    "13": {
+      name: "Haryana"
+    },
+    "14": {
+      name: "Himachal Pradesh"
+    },
+    "16": {
+      name: "Jharkhand"
+    },
+    "17": {
+      name: "Karnataka"
+    },
+    "18": {
+      name: "Kerala"
+    },
+    "19": {
+      name: "Lakshadweep"
+    },
+    "20": {
+      name: "Madhya Pradesh"
+    },
+    "21": {
+      name: "Maharashtra"
+    },
+    "22": {
+      name: "Manipur"
+    },
+    "23": {
+      name: "Meghalaya"
+    },
+    "24": {
+      name: "Mizoram"
+    },
+    "25": {
+      name: "Nagaland"
+    },
+    "26": {
+      name: "Orissa"
+    },
+    "27": {
+      name: "Puducherry"
+    },
+    "28": {
+      name: "Punjab"
+    },
+    "29": {
+      name: "Rajasthan"
+    },
+    "30": {
+      name: "Sikkim"
+    },
+    "31": {
+      name: "Tamil Nadu"
+    },
+    "32": {
+      name: "Tripura"
+    },
+    "33": {
+      name: "Uttar Pradesh"
+    },
+    "34": {
+      name: "Uttaranchal"
+    },
+    "35": {
+      name: "West Bengal"
+    },
+    "36": {
+      name: "Jammu And Kashmir"
+    },
+    "37": {
+      name: "Telangana"
+    },
+    "38": {
+      name: "Ladakh"
+    }
+  },
+  locations: {
+    "0": {
+      lat: 28.683831,
+      lng: 77.277606,
+      name: "Smile Foundation",
+      description: "Delhi Main Branch"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
